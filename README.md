9485001853cc1179a4f87b763293559c3d8cd0e0
khoo02 <khooiverson02@gmail.com>
2025-06-17 15:08:13 -0700

    add operator function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Calculator
 1. Create calculation functions: add, subtract, multiply, divide
+2. Create operator function to call calculation functions.
--- script.js
+++ script.js
@@ -24,3 +24,13 @@ function divide(num1, num2) {
 }
 
 // 2. Create a function to perform the calculation based on the operator
+function operate(num1, num2, operator) {
+    // use switch for case base operation
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+        default: alert('Unknown operation');
+    }
+}
